d2857ed6f54561b792040fad64ec181b4a7626bb
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-01-27 00:49:37 +0300

    using event bus

--- 1-blog/posts/index.js
+++ 1-blog/posts/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { randomBytes } = require('crypto')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const axios = require('axios')
 
 const app = express()
 app.use(bodyParser.json())
@@ -12,10 +13,17 @@ app.get('/posts', (req, res) => {
   res.send(posts)
 })
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
   const id = randomBytes(4).toString('hex')
   const { title } = req.body
   posts[id] = { id, title }
+
+  await axios
+    .post('http://localhost:4005', {
+      type: 'postCreated',
+      data: { id, title },
+    })
+    .catch((err) => console.log(err.message))
 
   res.status(201).send(posts[id])
 })
